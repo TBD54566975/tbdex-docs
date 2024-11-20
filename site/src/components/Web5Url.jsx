ae65b59a88770bfc6ac070ecd837df6b2baa837c
dayhaysoos <ndejesus1227@gmail.com>
2024-11-20 18:06:07 -0500

    fix url

--- site/src/components/Web5Url.jsx
+++ site/src/components/Web5Url.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Link from '@docusaurus/Link';
 
-export default function Web5Url({ to, children, ...props }) {
-  const {
-    siteConfig: { customFields },
-  } = useDocusaurusContext();
-
-  const web5DocsBaseUrl = customFields?.WEB5_DOCS_BASE_URL || '';
-
-  const fullUrl = `${web5DocsBaseUrl}${to}`;
+const Web5Url = ({ to, children }) => {
+  const baseUrl = 'http://web5_url_here/docs/web5';
+  const fullUrl = `${baseUrl}${to}`;
 
   return (
-    <Link to={fullUrl} {...props}>
+    <a href={fullUrl} target="_blank" rel="noopener noreferrer">
       {children}
-    </Link>
+    </a>
   );
-}
+};
 
 Web5Url.propTypes = {
   to: PropTypes.string.isRequired,
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
 
-Web5Url.defaultProps = {
-  children: null,
-};
+export default Web5Url;
